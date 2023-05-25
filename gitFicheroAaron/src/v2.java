f9b364a85130929ab355314f750771567cf62e2b
Aaron GIT <aarcaslea@alu.edu.gva.es>
2023-05-25 12:26:25 +0200

    Creado v2

---
+++ gitFicheroAaron/src/v2.java
@@ -0,0 +1,15 @@
+public class v2 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Gachamiga");
+            } else if (i % 3 == 0) {
+                System.out.println("Gacha");
+            } else if (i % 5 == 0) {
+                System.out.println("Miga");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
