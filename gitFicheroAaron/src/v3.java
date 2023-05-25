84c8ec9b87d01745bcbfa861264451f4d9d47ae5
Aaron GIT <aarcaslea@alu.edu.gva.es>
2023-05-25 12:48:36 +0200

    He hecho la primera parte de refactorizar

---
+++ gitFicheroAaron/src/v3.java
@@ -0,0 +1,16 @@
+public class v3 {
+    public static String gacha(String s) {
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Gachamiga");
+            } else if (i % 3 == 0) {
+                System.out.println("Gacha");
+            } else if (i % 5 == 0) {
+                System.out.println("Miga");
+            } else {
+                System.out.println(i);
+            }
+        }
+        return s;
+    }
+}
